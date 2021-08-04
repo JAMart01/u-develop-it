77cf6781466815264960ea5f429eab9128c3f0e7
JAMart <jorge.martinez1597@gmail.com>
2021-08-04 18:48:51 -0400

    updated database and seeds to include party affiliation for candidates

--- server.js
+++ server.js
@@ -24,7 +24,11 @@ const db = mysql.createConnection(
 
 // Get all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql =  `SELECT * FROM candidates`;
+    const sql =  `SELECT candidates.*, parties.name 
+                  AS party_name
+                  FROM candidates
+                  LEFT JOIN parties
+                  ON candidates.party_id = parties.id`;
 
     // Query request to get all candidates
     db.query(sql, (err, rows) => {
@@ -42,7 +46,12 @@ app.get('/api/candidates', (req, res) => {
 
 // Get a single candidate 
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+                 AS party_name 
+                 FROM candidates
+                 LEFT JOIN parties
+                 ON candidates.party_id = parties.id 
+                 WHERE candidates.id = ?`;
     const params = [req.params.id];
 
     // Query request to get a single candidate by id
